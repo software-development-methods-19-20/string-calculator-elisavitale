10e855b20f1cea1c4e837901b67d182cc507833b
elisavitale <elisavitale@live.it>
2019-11-13 10:58:41 +0100

    Tests for empty string, one number and two numbers.

--- test/source/test/AddNumbersTest.java
+++ test/source/test/AddNumbersTest.java
@@ -1,4 +1,23 @@
 package test;
 
+import dssc.calculator.StringCalculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AddNumbersTest {
+
+    @Test
+    void emptyString() {
+        assertEquals(0, StringCalculator.add(""));
+    }
+
+    @Test
+    void oneNumber() {
+        assertEquals(1, StringCalculator.add("1"));
+    }
+
+    @Test
+    void twoNumbers() {
+        assertEquals(3, StringCalculator.add("1,2"));
+    }
 }
