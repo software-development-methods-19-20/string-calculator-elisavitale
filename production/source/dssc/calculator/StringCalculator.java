34615234448af68dc5d13d5f3639618cb2321188
elisavitale <elisavitale@live.it>
2019-11-14 19:43:54 +0100

    Refactoring: the order of the methods was changed for more clarity.

--- production/source/dssc/calculator/StringCalculator.java
+++ production/source/dssc/calculator/StringCalculator.java
@@ -9,47 +9,47 @@ public class StringCalculator {
             return addAllNumbers(stringOfNumbers);
     }
 
+    public static int addAllNumbers(String string) {
+        int sum = 0;
+        for (String number : getAllNumbers(string))
+            sum += toInt(number);
+        return sum;
+    }
+
     public static int toInt(String string) {
         return Integer.valueOf(string);
     }
 
-    public static boolean thereAreDelimiters(String string) {
-        return containsNewDelimiters(string) || string.contains(",") || string.contains("\n");
+    public static String[] getAllNumbers(String string) {
+        if (thereAreDelimiters(string)) {
+            return ignoreFirstLine(string).split(getAllDelimiters(string));
+        } else return new String[] {string};
     }
 
-    public static boolean containsNewDelimiters(String string) {
+    public static boolean thereAreDelimiters(String string) {
+        return string.contains(",") || string.contains("\n") || containsNewDelimiter(string);
+    }
+
+    public static boolean containsNewDelimiter(String string) {
         if (string.startsWith("//") && string.contains("\n"))
             return true;
         else return false;
     }
 
-    public static String getNewDelimiter(String string) {
-        return string.substring(2, string.indexOf("\n"));
-    }
-
     public static String ignoreFirstLine(String string) {
-        if (containsNewDelimiters(string)) {
+        if (containsNewDelimiter(string)) {
             return string.substring(string.indexOf("\n") + 1);
         } else return string;
     }
 
-    public static String getSplittingString(String stringOfNumbers) {
-        if (containsNewDelimiters(stringOfNumbers)) {
-            return "[" + getNewDelimiter(stringOfNumbers) + ",\n]";
+    public static String getAllDelimiters(String string) {
+        if (containsNewDelimiter(string)) {
+            return "[" + getNewDelimiter(string) + ",\n]";
         } else return "[,\n]";
     }
 
-    public static String[] getAllNumbers(String stringOfNumbers) {
-        if (thereAreDelimiters(stringOfNumbers)) {
-            return ignoreFirstLine(stringOfNumbers).split(getSplittingString(stringOfNumbers));
-        } else return new String[] {stringOfNumbers};
-    }
-
-    public static int addAllNumbers(String stringOfNumbers) {
-        int sum = 0;
-        for (String number : getAllNumbers(stringOfNumbers))
-            sum += toInt(number);
-        return sum;
+    public static String getNewDelimiter(String string) {
+        return string.substring(2, string.indexOf("\n"));
     }
 
 }
