2cb0936c3dd5abe03ee9c638cdca02ee8ee9b618
elisavitale <elisavitale@live.it>
2019-11-14 19:26:01 +0100

    Refactoring: methods add and getAllNumbers now handle two cases instead of three.

--- production/source/dssc/calculator/StringCalculator.java
+++ production/source/dssc/calculator/StringCalculator.java
@@ -5,54 +5,51 @@ public class StringCalculator {
     public static int add(String stringOfNumbers) {
         if (stringOfNumbers.isEmpty()) {
             return 0;
-        } else if (thereAreManyNumbers(stringOfNumbers)) {
+        } else
             return addAllNumbers(stringOfNumbers);
-        } else {
-            return onlyOneNumber(stringOfNumbers);
-        }
     }
 
     public static int toInt(String string) {
         return Integer.valueOf(string);
     }
 
-    public static boolean thereAreManyNumbers(String string) {
+    public static boolean thereAreDelimiters(String string) {
         return containsNewDelimiters(string) || string.contains(",") || string.contains("\n");
     }
 
     public static boolean containsNewDelimiters(String string) {
-        if (string.startsWith("//"))
+        if (string.startsWith("//") && string.contains("\n"))
             return true;
         else return false;
     }
 
     public static String getNewDelimiter(String string) {
-        if (containsNewDelimiters(string))
-            return string.substring(2, string.indexOf("\n"));
-        else return "";
+        return string.substring(2, string.indexOf("\n"));
     }
 
     public static String ignoreFirstLine(String string) {
-        return string.substring(string.indexOf("\n") + 1);
+        if (containsNewDelimiters(string)) {
+            return string.substring(string.indexOf("\n") + 1);
+        } else return string;
+    }
+
+    public static String getSplittingString(String stringOfNumbers) {
+        if (containsNewDelimiters(stringOfNumbers)) {
+            return "[" + getNewDelimiter(stringOfNumbers) + ",\n]";
+        } else return "[,\n]";
     }
 
     public static String[] getAllNumbers(String stringOfNumbers) {
-        if (containsNewDelimiters(stringOfNumbers)) {
-            String newDelimiter = getNewDelimiter(stringOfNumbers);
-            return ignoreFirstLine(stringOfNumbers).split(newDelimiter);
-        }
-        else return stringOfNumbers.split("[,\n]");
+        if (thereAreDelimiters(stringOfNumbers)) {
+            return ignoreFirstLine(stringOfNumbers).split(getSplittingString(stringOfNumbers));
+        } else return new String[] {stringOfNumbers};
     }
 
     public static int addAllNumbers(String stringOfNumbers) {
         int sum = 0;
         for (String number : getAllNumbers(stringOfNumbers))
-            if (!number.equals(""))
-                sum += toInt(number);
+            sum += toInt(number);
         return sum;
     }
 
-    public static int onlyOneNumber(String stringOfNumbers) {
-        return toInt(stringOfNumbers);
-    }
 }
