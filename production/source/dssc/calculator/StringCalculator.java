8d44ae3a57df5f25cb4571d1b4c47d88071c267f
elisavitale <elisavitale@live.it>
2019-11-15 00:01:12 +0100

    Refactoring: changed the names of some variables and the order of some methods for clarity.

--- production/source/dssc/calculator/StringCalculator.java
+++ production/source/dssc/calculator/StringCalculator.java
@@ -11,27 +11,15 @@ public class StringCalculator {
 
     public static int addAllNumbers(String string) {
         int sum = 0;
-        for (String number : ignoreLargerThan1000(getAllNumbers(string)))
+        for (String number : getAllNumbers(string))
             sum += toInt(number);
         return sum;
     }
 
-    public static int toInt(String string) {
-        return Integer.valueOf(string);
-    }
-
     public static String[] getAllNumbers(String string) {
         if (thereAreDelimiters(string))
-            return ignoreFirstLine(string).split(getAllDelimiters(string));
+            return splitAndFilterNumbers(string);
         else return new String[] {string};
-    }
-
-    public static String[] ignoreLargerThan1000(String[] allNumbers) {
-        String newstring = "";
-        for (String number : allNumbers)
-            if (toInt(number) < 1000)
-                newstring += number + ",";
-        return newstring.split(",");
     }
 
     public static boolean thereAreDelimiters(String string) {
@@ -42,6 +30,22 @@ public class StringCalculator {
         if (string.startsWith("//") && string.contains("\n"))
             return true;
         else return false;
+    }
+
+    public static String[] splitAndFilterNumbers(String string) {
+        return ignoreLargerThan1000(splitNumbers(string));
+    }
+
+    public static String[] ignoreLargerThan1000(String[] allNumbers) {
+        String filteredNumbers = "";
+        for (String number : allNumbers)
+            if (toInt(number) < 1000)
+                filteredNumbers += number + ",";
+        return filteredNumbers.split(",");
+    }
+
+    public static String[] splitNumbers(String string) {
+        return ignoreFirstLine(string).split(getAllDelimiters(string));
     }
 
     public static String ignoreFirstLine(String string) {
@@ -58,6 +62,10 @@ public class StringCalculator {
 
     public static String getNewDelimiter(String string) {
         return string.substring(2, string.indexOf("\n"));
+    }
+
+    public static int toInt(String string) {
+        return Integer.valueOf(string);
     }
 
 }
